e027ec4b4596a3a866f67482d386303a78fc6e2e
junction2412 <julian@code-junction.de>
2025-04-23 19:57:32 +0200

    #13 moved classes to scala-3

--- src/test/scala-3/dev/junction/lab/ExpenseEntryControllerTests.scala
+++ src/test/scala-3/dev/junction/lab/ExpenseEntryControllerTests.scala
@@ -3,14 +3,14 @@ package dev.junction.lab
 import dev.junction.lab.controllers.ExpenseController
 import org.scalatra.test.scalatest.*
 
-class ExpenseEntryControllerTests extends ScalatraFunSuite {
+class ExpenseEntryControllerTests extends ScalatraFunSuite:
 
-  addServlet(classOf[ExpenseController], "/*")
+  addServlet(classOf[ExpenseController], "/expenses")
 
   test("GET / on WealthPocket should return status 200") {
     get("/") {
       status should equal (200)
     }
   }
-
-}
+  
+end ExpenseEntryControllerTests
